b337b636cf01c62eb0591cc6cf7e50c1b6df6525
Ruben Felgenhauer <4felgenh@informatik.uni-hamburg.de>
2021-02-03 14:49:59 +0100

    Add argument to select certain parts

--- plot-elf.py
+++ plot-elf.py
@@ -18,32 +18,50 @@ from elftools.common.exceptions import ELFError
 from elftools.elf.elffile import ELFFile
 from elftools.elf.segments import Segment
 import typing
-from typing import List, Dict, Tuple, Any, Collection
+from typing import List, Dict, Tuple, Any, Collection, Set
 
 
 # Workaround because numpy and the typing system don't work very well together atm...
 NpArray = Any
 
 
-def load_elf_files(filenames: Collection[str]) -> Tuple[Dict[str,NpArray],Dict[str,ELFFile]]:
+class PlottingOptions:
+    selected_parts: Set[str] = set()
+    strip: bool = False
+
+
+class ElfFileData:
+    byte_data: NpArray = None
+    elf_data: ELFFile = None
+
+
+def error(msg: str) -> None:
+    """
+    Print a message and quit with non-zero exit code.
+    :param msg: "Error: " + the message to print.
+    """
+    print("Error: {}".format(msg))
+    sys.exit(1)
+
+
+def load_elf_files(filenames: Collection[str]) -> Dict[str,ElfFileData]:
     """
     Load the ELF files and read their ELF data and byte data.
     :param filenames: The names of the files to load.
-    :return: A dict from filename to the bytes as numpy arrays with dtype "uint8",
-             and a dict from filename to the ELF data.
+    :return: A dict from filename to ElfFileData objects, which contain the
+             ELF data and byte data.
     """
-    data = dict()
-    elf = dict()
+    result: Dict[str,ElfFileData] = dict()
     for f in filenames:
         with open (f, "rb") as elffile:
             raw_data = elffile.read()
         try:
-            data[f] = np.frombuffer(raw_data, dtype="uint8")
-            elf[f] = ELFFile(io.BytesIO(raw_data))
+            result[f] = ElfFileData()
+            result[f].byte_data = np.frombuffer(raw_data, dtype="uint8")
+            result[f].elf_data = ELFFile(io.BytesIO(raw_data))
         except ELFError:
-            print("Error: Not a valid ELF file: \"{}\"".format(f))
-            sys.exit(1)
-    return data, elf
+            error("Not a valid ELF file: \"{}\"".format(f))
+    return result
 
 
 def get_max_length(arrays: Collection[Collection]) -> int:
@@ -52,9 +70,9 @@ def get_max_length(arrays: Collection[Collection]) -> int:
     :param arrays: The collections to analyze.
     :return: The maximum length.
     """
-    max_length = 0
+    max_length: int = 0
     for a in arrays:
-        size = len(a)
+        size: int = len(a)
         if size > max_length:
             max_length = size
     return max_length
@@ -67,7 +85,7 @@ def pad_array(array: NpArray, length: int) -> NpArray:
     :param length: The length to pad the array to.
     :return: The padded array.
     """
-    result = np.zeros(length, dtype=array.dtype)
+    result: NpArray = np.zeros((length,3), dtype=array.dtype)
     result[:len(array)] = array
     return result
 
@@ -78,7 +96,7 @@ def primes(n: int) -> List[int]:
     :param n: The number.
     :return: The prime factorization as a list with repeated factors.
     """
-    primfac = []
+    primfac: List[int] = []
     d = 2
     while d*d <= n:
         while (n % d) == 0:
@@ -99,6 +117,8 @@ def get_optimal_color_division(num_colors: int) -> int:
     :param num_colors: The number of colors.
     :return: The step width with which to go through the list of colors.
     """
+    if (num_colors < 7):
+        return 1
     division = int((num_colors - 1) / 3)
     while (division < num_colors):
         if not set(primes(division)).issubset(set(primes(num_colors))):
@@ -106,111 +126,158 @@ def get_optimal_color_division(num_colors: int) -> int:
     return num_colors - 1
 
 
-def colorize_data(data: Dict[str,NpArray], elf: Dict[str,NpArray]) -> Tuple[Dict[str,NpArray],Dict[str,List[str]],Dict[str,Line2D]]:
+def colorize_data(elf_files: Dict[str,ElfFileData], options: Dict[str,PlottingOptions]) -> Dict[str,List[Tuple[str,Line2D]]]:
     """
     Colorize the ELF header, Program Header, Section Header, and various sections
     (e.g. .text, .date, …) of the given bytes of the ELF Object Code Files with
-    different colors on the HSV spectrum and prepeare a legend for the plot.
-    :param data: A dict from filename to the bytes as numpy arrays with dtype "uint8".
-    :param elf: A dict from filename to the corresponding ELF data of the files.
-    :return: A dict from filename to the colorized (RGB) data,
-             a dict from filename to the names of the parts of the data,
-             and a dict from filename to the Line2D objects containing the
-             colors of the parts of the data.
+    different colors on the HSV spectrum and prepare a legend for the plot.
+    :param elf_files: A dict from filename to ElfFileData objects that contains
+                      the ELF and byte data.
+    :param options: A dict from filename to PlottingOptions objects which
+                    determine selected sections and stripping.
+    :return: A dict from filename to the names of the parts of the data and to
+             the Line2D objects containing the colors of the parts of the data.
     """
-    res_data = dict()
-    legend_names = dict()
-    legend_colors = dict()
-    for f in data.keys():
-        curr_elf = elf[f]
-        curr_data = data[f]
-        curr_data = np.stack((curr_data,curr_data,curr_data), axis=1)
+    legend_data: Dict[str,List[Tuple[str,Line2D]]] = dict()
+    for f in elf_files:
+        elf: ELFFile = elf_files[f].elf_data
+        bytes: NpArray = elf_files[f].byte_data
+        selected_parts: Set[str] = options[f].selected_parts
+        strip: bool = options[f].strip
 
-        part_names = ["Ehdr"]
-        part_offsets = [0]
-        part_lengths = [curr_elf.header.e_ehsize]
-        for s in curr_elf.iter_sections():
+        bytes = np.stack((bytes,bytes,bytes), axis=1)
+
+        part_names: List[str] = ["Ehdr"]
+        part_offsets: List[int] = [0]
+        part_lengths: List[str] = [elf.header.e_ehsize]
+        for s in elf.iter_sections():
             part_names.append(s.name)
             part_offsets.append(part_offsets[-1] + part_lengths[-1])
             part_lengths.append(s.data_size)
         part_names.extend(["Phdr", "Shdr"])
-        part_offsets.extend([curr_elf.header.e_phoff, curr_elf.header.e_shoff])
-        part_lengths.extend([curr_elf.header.e_phentsize, curr_elf.header.e_shentsize])
+        part_offsets.extend([elf.header.e_phoff, elf.header.e_shoff])
+        part_lengths.extend([elf.header.e_phentsize, elf.header.e_shentsize])
 
         parts = list(zip(part_names, part_offsets, part_lengths))
         parts = [(name,offset,length) for (name,offset,length) in parts if length > 0]
-        num_parts = len(parts)
+        if selected_parts != []:
+            parts = [(name,offset,length) for (name,offset,length) in parts if (name in selected_parts)]
 
+        num_parts = len(parts)
         colors = cm.rainbow(np.linspace(0, 1, num_parts))
         colors = colors[:,0:3]
 
-        curr_legend_names = []
-        curr_legend_colors = []
+        current_legend_data = []
         color_division = get_optimal_color_division(num_parts)
         for i, (name, offset, length) in enumerate(parts):
             j = (i * color_division) % (num_parts)
-            curr_data[offset : offset+length] = (curr_data[offset : offset+length] * colors[j]).astype("uint8")
-            curr_legend_colors.append(Line2D([0], [0], color=colors[j], lw=4))
-            curr_legend_names.append(name)
-        res_data[f] = curr_data
-        legend_names[f] = curr_legend_names
-        legend_colors[f] = curr_legend_colors
-    return res_data, legend_names, legend_colors
+            bytes[offset : offset+length] = (bytes[offset : offset+length] * colors[j]).astype("uint8")
+            current_legend_data.append((name, Line2D([0], [0], color=colors[j], lw=4)))
+
+        legend_data[f] = current_legend_data
+
+        if strip:
+            stripped_bytes = np.empty((0,3), dtype="uint8")
+            for i, (name, offset, length) in enumerate(parts):
+                stripped_bytes = np.append(stripped_bytes, bytes[offset : offset+length], axis=0)
+            bytes = stripped_bytes
+        elf_files[f].byte_data = bytes
+
+    return legend_data
 
 
-def plot_elf_files(data: Dict[str,NpArray], elf: Dict[str,ELFFile], legend_names: Dict[str,List[str]], legend_colors: Dict[str,Line2D]) -> None:
+def plot_elf_files(elf_files: Dict[str,ElfFileData], legend_data: Dict[str,List[Tuple[str,Line2D]]]) -> None:
     """
     Plot the given ELF files.
-    :param data: A dict from filename to colorized data of the files.
-    :param elf: A dict from filename to the corresponding ELF data.
-    :param legend_names: A dict from filename to the names of the parts of the file.
-    :param legend_colors: A dict from filename to Line2D objects with colors of
-                          the parts of the file.
+    :param elf_files: A dict from filename to ElfFileData objects that contains
+                      the ELF and byte data.
+    :param legend_data: A dict from filename to the names of the parts of the
+                        data and to the Line2D objects containing the colors of
+                        the parts of the data.
+
     """
-    num_plots = len(data)
+    num_plots = len(elf_files)
     fig, ax = plt.subplots(ncols=num_plots)
-    for i, f in enumerate(data):
+    for i, f in enumerate(elf_files):
+        colorized_bytes = elf_files[f].byte_data
+        elf = elf_files[f].elf_data
         cax = ax[i] if num_plots > 1 else ax
-        colorized_data = data[f]
-        sqrt_len = int(math.sqrt(len(colorized_data)))
+        sqrt_len = int(math.sqrt(len(colorized_bytes)))
         w = int(sqrt_len / math.sqrt(2) / 8) * 8
-        h = int(len(colorized_data) / w)
-        colorized_data = colorized_data[:(w*h)]
-        colorized_data = colorized_data.reshape(h,w,3)
-        cax.imshow(colorized_data)
-        compiler = elf[f].get_section_by_name(".comment").data().decode("utf-8").strip('\x00')
+        h = int(len(colorized_bytes) / w)
+        colorized_bytes = colorized_bytes[:(w*h)]
+        colorized_bytes = colorized_bytes.reshape(h,w,3)
+        cax.imshow(colorized_bytes)
+        compiler = elf.get_section_by_name(".comment").data().decode("utf-8").strip('\x00')
         cax.set_title("{}\n[{}]".format(f, compiler))
-        cax.legend(legend_colors[f], legend_names[f], loc=(1.04,0))
+        cax.legend([x[1] for x in legend_data[f]], [x[0] for x in legend_data[f]], loc=(1.04,0))
     mng = fig.canvas.manager
     mng.window.showMaximized()
     plt.show()
 
 
-def main() -> None:
+def parse_args() -> Dict[str,PlottingOptions]:
+    """
+    Parse the arguments.
+    :return: A dict from filename to PlottingOptions objects which
+             determine selected sections and stripping.
+    """
+    args: List[str] = sys.argv[1:]
+    if args == []:
+        error("No filenames given.")
+    result: Dict[str,PlottingOptions] = {}
+    current_filename: str = ""
+    global_strip: bool = False
+    global_selected_parts: Set[str] = set()
+    for arg in args:
+        if arg.startswith("+"):
+            try:
+                if arg.startswith("++"):
+                    current_selected_parts = arg[2:].split(",")
+                    current_strip = True
+                else:
+                    current_selected_parts = arg[1:].split(",")
+                    current_strip = False
+                if current_filename == "":
+                    global_selected_parts.update(set(current_selected_parts))
+                    global_strip |= current_strip
+                else:
+                    result[current_filename].selected_parts.update(set(current_selected_parts))
+                    result[current_filename].strip |= current_strip
+            except:
+                error("Could not parse the list \"{}\"".format(arg))
+        elif os.path.isfile(arg):
+            if arg in result:
+                error("Specified filename twice: \"{}\"".format(arg))
+            current_filename = arg
+            result[current_filename] = PlottingOptions()
+        else:
+            error("Not a valid file: \"{}\"".format(arg))
+    if global_selected_parts != []:
+        for filename in result:
+            result[filename].selected_parts.update(set(global_selected_parts))
+            result[filename].strip |= global_strip
+    return result
 
-    filenames = sys.argv[1:]
-    if filenames == []:
-        print("Error: No filenames given.")
-        sys.exit(1)
-    for filename in filenames:
-        if not os.path.isfile(filename):
-            print("Error: Not a valid file: \"{}\"".format(filename))
-            sys.exit(1)
+
+def main() -> None:
+    options = parse_args()
+    filenames = options.keys()
 
     # Load the object code files as bytes and ELF data
-    data, elf = load_elf_files(filenames)
-
-    # Get the maximum length and pad each array to this value
-    max_length = get_max_length(data.values())
-    for f in filenames:
-        data[f] = pad_array(data[f], max_length)
+    elf_files = load_elf_files(filenames)
 
     # Colorize the data so that the ELF header, .text, .data, … all have
     # different colors.
-    data, legend_names, legend_colors = colorize_data(data, elf)
+    legend_data = colorize_data(elf_files, options)
+
+    # Get the maximum length and pad each array to this value
+    max_length = get_max_length([e.byte_data for e in elf_files.values()])
+    for f in filenames:
+        elf_files[f].byte_data = pad_array(elf_files[f].byte_data, max_length)
 
     # Plot the arrays.
-    plot_elf_files(data, elf, legend_names, legend_colors)
+    plot_elf_files(elf_files, legend_data)
 
 
 if __name__ == '__main__':
